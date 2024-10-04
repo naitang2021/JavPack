7d11716b44fba214acc3f1a3041009f474a04c35
naitang2021 <87680882+naitang2021@users.noreply.github.com>
2024-10-04 19:54:21 +0800

    Update JavPack.Req.lib.js

--- libs/JavPack.Req.lib.js
+++ libs/JavPack.Req.lib.js
@@ -1,5 +1,5 @@
 class Req {
-  static defaultDetails = { method: "GET", timeout: 30000 };
+  static defaultDetails = { method: "GET", timeout: 100000 };
 
   static isPlainObj = (obj) => Object.prototype.toString.call(obj) === "[object Object]";
 
